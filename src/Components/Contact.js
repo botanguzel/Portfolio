26656f4fc42f85ac7be4a265c248d608953bc78a
Botan Guzel <botanguzel@hotmail.com>
2023-09-20 11:17:57 +0200

    Fix issues

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { Container, Button, Form, Row, Col } from 'react-bootstrap';
+import React, { useEffect } from 'react';
+import { Container, Row } from 'react-bootstrap';
 import contactContent from './Contents/contactContent';
 import '../Styles/contact.css';
 
@@ -27,14 +27,14 @@ const Contact = () => {
                   <li style={{marginBottom: '3vh'}}> <a href={`mailto:${contact.email}`} className="text-white-50 bi bi-envelope-at-fill cList"> {contact.email} </a> </li>
                   <li style={{marginBottom: '3vh'}}> <a href={`tel:${contact.number}`} className="text-white-50 bi bi-phone-fill cList"> {contact.number} </a> </li>
                   <li style={{marginBottom: '3vh'}}> <a href={`https://maps.google.com/?q=${encodeURIComponent(contact.address)}`}
-                          className='text-white-50 bi bi-house-fill cList' target="_blank">
+                          className='text-white-50 bi bi-house-fill cList' target="_blank" rel='noreferrer'>
                             {contact.address}
                         </a>
                   </li>
                   <li style={{marginBottom: '3vh'}}> <a href={`${contact.linkedin}`} className="text-white-50 bi bi-linkedin cList"
-                    target='_blank'> {contact.linkedin} </a> </li>
+                    target='_blank' rel='noreferrer'> {contact.linkedin} </a> </li>
                   <li style={{marginBottom: '3vh'}}> <a href={`${contact.github}`} className="text-white-50 bi bi-github cList"
-                    target='_blank'> {contact.github} </a> </li>
+                    target='_blank' rel='noreferrer'> {contact.github} </a> </li>
                 </ul>
               </div>
             </Container>
